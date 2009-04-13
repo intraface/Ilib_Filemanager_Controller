d35aefbb9aac4af427c4767c9fd1c25afbd23ec6
lsolesen <lsolesen@2fc64ab5-e821-0410-98e1-ee8f871d7b73>
2009-04-13 19:39:47 +0000

    More sensible include path and added gateway to the path.

--- src/www/config.local.example.php
+++ src/www/config.local.example.php
@@ -9,8 +9,11 @@ define('PATH_WWW', 'http://localhost/vih/intranet/src/www/');
 define('PATH_UPLOAD', PATH_ROOT . 'upload\\');
 define('PATH_UPLOAD_ORIGINAL', PATH_ROOT . 'devel\original\\');
 define('PATH_UPLOAD_INSTANCE', PATH_ROOT . 'devel\instance\\');
-define('PATH_INCLUDE', 'c:/Users/Lars Olesen/workspace/ilib/DBQuery/src/' . PATH_SEPARATOR . 'c:/Users/Lars Olesen/workspace/vih/hojskole/src/' . PATH_SEPARATOR . 'c:/Users/Lars Olesen/workspace/ilib/Redirect/src/' . PATH_SEPARATOR .
-        'c:/Users/Lars Olesen/workspace/konstrukt/lib/' . PATH_SEPARATOR .
+define('PATH_INCLUDE', dirname(__FILE__) . '/../../../Ilib_Filemanager/src/' . PATH_SEPARATOR .
+        dirname(__FILE__) . '/../../../Ilib_Error/src/' . PATH_SEPARATOR .
+        dirname(__FILE__) . '/../../../Ilib_Redirect/src/' . PATH_SEPARATOR .
+        dirname(__FILE__) . '/../../../Ilib_Keyword/src/' . PATH_SEPARATOR .
+        dirname(__FILE__) . '/../../../Ilib_Keyword_Controller/src/' . PATH_SEPARATOR .
         dirname(__FILE__) . '/../' . PATH_SEPARATOR . ini_get('include_path'));
 define('FILE_VIEWER', '/vih/hojskole/src/vih.dk/file.php');
 define('PATH_UPLOAD_TEMPORARY', 'tempdir/');
