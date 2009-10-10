e6585bad02d092e19f167f9903f1f46bd6d2fcb7
lsolesen <lsolesen@2fc64ab5-e821-0410-98e1-ee8f871d7b73>
2009-10-10 09:55:23 +0000

    Fixed bug with delete on wrong object.

--- 3Party/Ilib_Filemanager_Controller/src/Intraface/Filehandler/Controller/Delete.php
+++ 3Party/Ilib_Filemanager_Controller/src/Intraface/Filehandler/Controller/Delete.php
@@ -7,7 +7,7 @@ class Intraface_Filehandler_Controller_Delete extends k_Controller
         $module = $kernel->module('filemanager');
         $translation = $kernel->getTranslation('filemanager');
 
-        $filemanager = new Ilib_Filehandler_Manager($kernel, $this->context->name);
+        $filemanager = new Ilib_Filehandler($kernel, $this->context->name);
         if (!$filemanager->delete()) {
             trigger_error($this->__('could not delete file'), E_USER_ERROR);
         }
